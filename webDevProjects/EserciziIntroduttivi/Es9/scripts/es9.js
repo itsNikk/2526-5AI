5f43535517de0848680008b6b743f044d62fc238
Nicolò Buganza <nicolo.buganza@tiscali.it>
2025-11-06 12:32:17 +0100

    end

--- webDevProjects/EserciziIntroduttivi/Es9/scripts/es9.js
+++ webDevProjects/EserciziIntroduttivi/Es9/scripts/es9.js
@@ -1,18 +1,28 @@
-let names = ["AURORA", "cristiano", "nicolò", "pietro"]
+// Prendere i nomi dal DOM tramite input text. aggiungere un bottone che, ogni volta che vien cliccato, aggiunge quanto 
+// c'è nella input al vettore
+let names = [];
 
 
-let resObj = document.getElementById("res")
+let resObj = document.getElementById("res");
+let btn = document.getElementById("btn");
 //find max. le str sono array
 //foreach JS 
 // per ogni elem della collezione names. for non indicizzato
 // [5,2,9,7]
 
-let maxName = names[0]
-for (elem of names) {
-    if (elem.length > maxName.length) {
-        maxName = elem
-    }
-}
 
-//show result
-resObj.textContent = maxName
+let maxName = names[0];
+// ES6
+/// Funzioni anonima (lambda functions)
+//function x(){} -> () => {} //programmaizone funzionale
+
+btn.addEventListener("click", () => {
+    for (let elem of names) {
+        if (elem.length > maxName.length) {
+            maxName = elem
+        }
+    }
+
+    //show result
+    resObj.textContent = maxName;
+})
