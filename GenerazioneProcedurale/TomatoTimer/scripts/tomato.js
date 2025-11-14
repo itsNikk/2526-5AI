8ae5f865300d7900124568ef454a2c2d9e9ccd24
Nicolò Buganza <nicolo.buganza@tiscali.it>
2025-11-14 15:40:12 +0100

    init tomato

---
+++ GenerazioneProcedurale/TomatoTimer/scripts/tomato.js
@@ -0,0 +1,39 @@
+let display = document.getElementById("timer")
+let startBtn = document.getElementById("startBtn")
+
+let timer = {
+    seconds: 1500,
+    running: false
+}
+
+//ctrl+alt+l = auto-format
+function updateView() {
+    let min = parseInt(timer.seconds / 60)
+    let sec = timer.seconds % 60
+
+    display.textContent = min + ":" + sec
+}
+
+//alt+shift+frecce
+function tick() {
+    timer.seconds--;
+    updateView()
+}
+
+function start() {
+    if (!timer.running) {
+        setInterval(tick, 1000);
+        timer.running = true
+    }
+    //SetInterval(p1, p2):
+    // esegui p1 ogni p2 ms
+    //SetTimeout(p1, p2):
+    //esegue p1 DOPO p2 ms (ONESHOT)
+}
+
+
+// Se la funzione è corta allora lambda
+// altrimenti normale
+startBtn.addEventListener("click", start)
+
+
