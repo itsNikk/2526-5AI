c78080fbb0a54e426b0693a28e827edb5832ae7b
Nicolò Buganza <nicolo.buganza@tiscali.it>
2025-11-14 15:47:08 +0100

    Update tomato.js

--- GenerazioneProcedurale/TomatoTimer/scripts/tomato.js
+++ GenerazioneProcedurale/TomatoTimer/scripts/tomato.js
@@ -1,3 +1,7 @@
+//1) fare reset
+//2) pensare al pause 
+
+
 let display = document.getElementById("timer")
 let startBtn = document.getElementById("startBtn")
 
@@ -14,7 +18,7 @@ function updateView() {
     display.textContent = min + ":" + sec
 }
 
-//alt+shift+frecce
+//alt+shift+frecce = sposto tutta una riga sopra o sotto
 function tick() {
     timer.seconds--;
     updateView()
@@ -27,6 +31,7 @@ function start() {
     }
     //SetInterval(p1, p2):
     // esegui p1 ogni p2 ms
+    // esegui p1 ogni p2 ms
     //SetTimeout(p1, p2):
     //esegue p1 DOPO p2 ms (ONESHOT)
 }
@@ -35,5 +40,6 @@ function start() {
 // Se la funzione è corta allora lambda
 // altrimenti normale
 startBtn.addEventListener("click", start)
+resetBtn.addEventListener("click", reset)
 
 
