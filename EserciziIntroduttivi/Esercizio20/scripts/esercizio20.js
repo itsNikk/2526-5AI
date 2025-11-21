51fe7de0460b07283be577e3e5169808b3f35790
Nicolò Buganza <nicolo.buganza@tiscali.it>
2025-11-21 15:09:31 +0100

    Update esercizio20.js
    cazzaniga update

--- EserciziIntroduttivi/Esercizio20/scripts/esercizio20.js
+++ EserciziIntroduttivi/Esercizio20/scripts/esercizio20.js
@@ -2,14 +2,25 @@ let btn = document.getElementById("genBtn")
 let notificationContainer = document.getElementById("notificationContainer")
 
 let index = 1;
+
+//for fun
+setInterval(() => {
+    index = 0;
+}, 10000);
+
 btn.addEventListener("click", () => {
     let newNotification =
         document.createElement("div")
 
     newNotification.textContent = "NOTIFICA #" + index++
 
+    newNotification.addEventListener("click", () => {
+        index = 0;
+    })
+
     notificationContainer.append(newNotification)
 
-
-    setTimeout(() => { newNotification.remove() }, 3000)
+    setTimeout(() => {
+        newNotification.remove()
+    }, 3000)
 })
