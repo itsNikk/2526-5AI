37bafc6e2010b4b871a561073bd856a81f1a5d7e
Nicolò Buganza <nicolo.buganza@tiscali.it>
2026-01-16 15:24:30 +0100

    Update server.js

--- NodeProjects/introNode/server.js
+++ NodeProjects/introNode/server.js
@@ -8,8 +8,13 @@ const server = http.createServer((req, res) => {
 
     if (req.url === "/") {
         res.statusCode = 200;
+        //HEADER NAME - MIME Type
         res.setHeader("Content-Type", "text/plain");
         res.end("Ciaoo, benvenuto!");
+    } else {
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ error: req.url + " Not found" }))
     }
 
 })
