26c13e9f9c6e8687ace2784c4d7b58e7f2f72380
iTzNikk <nicolo.buganza@tiscali.it>
2026-01-25 21:19:51 +0100

    Update asyncEs3.js

--- AsyncJSExercises/asyncEs3/scripts/asyncEs3.js
+++ AsyncJSExercises/asyncEs3/scripts/asyncEs3.js
@@ -6,7 +6,8 @@ fetch(URL)
     .then(output => console.log(output[0] + ", " + output[1] + "kg"))
     .catch(err => console.log(err.message));
 
+//Per comodità. Se in futuro dovrò estrarre altre feature modifcherò più facilmente una funzione invece che un .then()
 function extractFeatures(data) {
-    console.log(data);
+    //console.log(data);
     return [data.name, data.weight]
 }
