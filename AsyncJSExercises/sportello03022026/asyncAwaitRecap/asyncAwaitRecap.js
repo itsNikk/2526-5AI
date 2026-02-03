2724bf25f83872e85b5c1d18eec1b6cba29db731
Nicolò Buganza <nicolo.buganza@tiscali.it>
2026-02-03 14:46:03 +0100

    Update asyncAwaitRecap.js

--- AsyncJSExercises/sportello03022026/asyncAwaitRecap/asyncAwaitRecap.js
+++ AsyncJSExercises/sportello03022026/asyncAwaitRecap/asyncAwaitRecap.js
@@ -1,15 +1,15 @@
 /*
 * ESERCIZIO: 
-* Scrivi una fnzione che:
+* Scrivi una funzione che:
     1. Recupera un utente a caso da jsonplaceholder
     2. Use l'email dello user per recuperare tutti i suoi post
     3. Per ogni post ottieni i commenti
     4. stampa in console: nome utente, titolo del post e quanti commenti ha ricevuto
 
-    VINCOLO:
+    * VINCOLI:
     1. preferire async/await
-    2. 
-
+    2. ...
     */
 
 const URL = "https://jsonplaceholder.typicode.com/users"
+
