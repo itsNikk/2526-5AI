58f4baac9f71c64739805462e2960aec3f507a0d
Nicolò Buganza <nicolo.buganza@tiscali.it>
2026-02-03 15:50:30 +0100

    user posts

--- AsyncJSExercises/sportello03022026/asyncAwaitRecap/asyncAwaitRecap.js
+++ AsyncJSExercises/sportello03022026/asyncAwaitRecap/asyncAwaitRecap.js
@@ -4,12 +4,48 @@
     1. Recupera un utente a caso da jsonplaceholder
     2. Use l'email dello user per recuperare tutti i suoi post
     3. Per ogni post ottieni i commenti
-    4. stampa in console: nome utente, titolo del post e quanti commenti ha ricevuto
+    4. stampa in console: nome utente, titolo del primo post e quanti commenti ha ricevuto
 
     * VINCOLI:
     1. preferire async/await
     2. ...
     */
 
-const URL = "https://jsonplaceholder.typicode.com/users"
+const URL = "https://jsonplaceholder.typicode.com/users/"
 
+async function getUserInfo() {
+
+    // * 1. Recuperiamo un utente a caso da server
+    const randomUserId = Math.floor((Math.random() * 10) + 1);
+    // Fetch è una Promise
+    const userResponse = await fetch(URL + randomUserId);
+    console.log(userResponse);
+    const user = await userResponse.json();
+    console.log(user);
+
+    console.log("Utente trovato: " + user.name);
+    console.log("Email: " + user.email);
+
+    // *2. recuperiamo tutti i post dei quell'utente
+    const postsResponse = await fetch("https://jsonplaceholder.typicode.com/posts?userId=" + randomUserId)
+    const posts = await postsResponse.json();
+
+    if (posts.length === 0) {
+        console.log("Nessun post trovato");
+    }
+
+    console.log("Numero tot di posts: " + posts.length);
+    console.log("Titolo primo post: " + posts[0].title);
+
+
+}
+
+
+
+//Content Negotiation : Accept (C) e Content-Type (CS)
+
+
+
+
+
+getUserInfo()
