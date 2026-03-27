11704c16a82d579f238fb07af3173c1949c5a95c
Nicolò Buganza <nicolo.buganza@tiscali.it>
2026-03-27 15:17:23 +0100

    Update eisleyServer.js

--- ExpressProjekts/ExpressEisley/eisleyServer.js
+++ ExpressProjekts/ExpressEisley/eisleyServer.js
@@ -2,6 +2,8 @@ const express = require("express")
 // Creo il servizio...
 const app = express()
 
+app.use(express.json())
+
 let clienti = [
     { id: 1, nome: "Han Solo", specie: "umano", credito: 1500 },
     { id: 2, nome: "Greedo", specie: "rodiano", credito: 350 },
@@ -10,6 +12,14 @@ let clienti = [
 ]
 
 let nexClientId = clienti.length + 1
+
+// MW = (req,res,next)
+// Global: printare methodo e url della res
+app.use((req, res, next) => {
+
+    console.log();
+
+})
 
 app.listen(3000, () => {
     console.log("Cantina aperta su porta " + 3000);
